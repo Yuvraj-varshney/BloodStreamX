1a01c3dcea2ac84126f602071ccfd28124933c59
Yuvraj <yuvrajvarshney2021@gmail.com>
2024-04-05 20:19:27 +0530

    all users lists accessed

---
+++ client/src/pages/Admin/HospitalList.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import moment from "moment";
+import API from "../../services/API";
+
+const HospitalList = () => {
+  const [data, setData] = useState([]);
+  //find donar records
+  const getDonars = async () => {
+    try {
+      const { data } = await API.get("/admin/hospital-list");
+      console.log(data);
+      if (data?.success) {
+        setData(data?.hospitalData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDonars();
+  }, []);
+
+
+
+  return (
+    <Layout>
+      <table className="table ">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Date</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((record) => (
+            <tr key={record._id}>
+              <td>{record.hospitalName}</td>
+              <td>{record.email}</td>
+              <td>{record.phone}</td>
+              <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+          
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
+
+export default HospitalList;
